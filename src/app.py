d69e24ec236d3902c65fc452214f6705fabd8bae
SofiaBreslavets <yev25001@uconn.edu>
2026-04-28 02:04:36 +0000

    Add 2 more sports related activities, 2 more artistic activities, and 2 more intellectual activities.

--- src/app.py
+++ src/app.py
@@ -69,4 +69,4 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Add student
     activity["participants"].append(email)
-    return {"message": f"Signed up {email} for {activity_name}"}}
+    return {"message": f"Signed up {email} for {activity_name}"}
